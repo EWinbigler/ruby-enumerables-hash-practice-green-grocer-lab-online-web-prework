cf655aaa842ab1ed1b9d90e14ad143f9f68e080e
Eric Winbigler <uzof99@gmail.com>
2019-07-03 11:56:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,21 @@
 def consolidate_cart(cart)
-  
+  consolidated = Hash.new(0)
+  cart.each do |whole_cart| 
+    whole_cart.each do |each_item|
+      item = each_item[0]
+      if consolidated.include?(item)
+        consolidated[item][:count] += 1
+      else
+        consolidated[item] = each_item[1]
+        consolidated[item][:count] = 1
+      end
+    end
+  end
+  consolidated
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
 end
 
 def apply_clearance(cart)
